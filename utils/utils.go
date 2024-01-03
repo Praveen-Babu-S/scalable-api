57c7b213fde049c1412dde27f91bcfc4859afaaf
Praveen <praveen.babu@sojern.com>
2024-01-03 19:46:38 +0530

    - add util function - function marshal any data to string

--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,14 @@
 package utils
+
+import "encoding/json"
+
+func MarshalToString(data interface{}) string {
+	if data == nil {
+		return ""
+	}
+	jsonString, err := json.Marshal(data)
+	if err != nil {
+		return ""
+	}
+	return string(jsonString)
+}
